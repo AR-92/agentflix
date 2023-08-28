02678e0de91809a10197ddbb180c949095a09a11
AR-92 <electro.electra@gmail.com>
2023-08-28 14:12:46 +0500

    options

--- src/routes/privacyOptions/+page.svelte
+++ src/routes/privacyOptions/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <MiniNav />
-<main class="flex flex-col gap-4 p-10 text-left ">
+<main class="flex flex-col gap-4 p-10 text-left card m-20">
 	<h2 class="h3 font-medium mb-6">Your privacy choices</h2>
 	<h2 class="h4 font-medium mb-3">Opt out of selling, sharing and targeted advertising</h2>
 	<p>Airbnb does not sell personal information to third parties: we’re not a data broker and we don’t put personal information on the open market. However, we may share personal information with certain third parties to do targeted advertising or data analytics, which under California and certain other state privacy laws could be characterized as “selling,” “sharing,” or “targeted advertising”.</p>
