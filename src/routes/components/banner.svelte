80874152425803f1438f5e55449a3a3fd84256e6
AR-92 <electro.electra@gmail.com>
2023-09-01 04:36:45 +0500

    default

--- src/routes/components/banner.svelte
+++ src/routes/components/banner.svelte
@@ -4,6 +4,7 @@
 	<div class="grid grid-cols-12 py-11">
 		<div class="col-span-4"></div>
 		<div class="col-span-7 ">
+			<div class="text-xs mt-5 dark:text-primary-100 text-primary-700 mb-2">This is your defaul banner , You can change that from settings !</div>
 			<div class="text-5xl dark:text-primary-100 text-primary-700">Banner Space: Elevate Your Message with Impactful Design</div>
 			<div class="text-xs mt-5 dark:text-primary-100 text-primary-700">Banner Space is your go-to destination for professional design services. Our team specializes in creating eye-catching banners that not only capture attention but also convey your message effectively. With a keen eye for aesthetics and a commitment to quality, we help businesses and individuals make a memorable impact. Whether it's for marketing campaigns, events, or promotions, trust Banner Space to elevate your visual communication and leave a lasting impression on your audience. Explore our expertise today and let your message shine.</div>
 		</div>
