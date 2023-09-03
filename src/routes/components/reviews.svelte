feafaf44e6c7f7e7e96df5dac912289135fd0ea7
AR-92 <electro.electra@gmail.com>
2023-09-03 15:32:48 +0500

    database

--- src/routes/components/reviews.svelte
+++ src/routes/components/reviews.svelte
@@ -2,6 +2,7 @@
 	import { RangeSlider } from '@skeletonlabs/skeleton';
 	import { userdata } from '../store/userStore';
 	import Signup from './joinNow.svelte';
+	import { supabase } from '$lib/supabaseClient';
 
 	import Model from './model.svelte';
 	import Events from '../icons/emptyReviews.svelte';
@@ -10,43 +11,30 @@
 	let value = 4;
 	let max = 5;
 	let openSignup = false;
-
-	var reviews = [
-		{
-			name: 'John Baker',
-			date: '06/01/2021',
-			ratings: '4.5',
-			comment:
-				'Tonya Currie & her team resides in Washington & Grimes County areas and represent some of the most fascinating properties throughout Texas which have been featured in the Wall Street Journal, Paper City, Houston Luxury, & Houston Chronicle.',
-			type: 'Highly likely to recommend'
-		},
-		{
-			name: 'John Baker',
-			date: '06/01/2021',
-			ratings: '4.5',
-			comment:
-				'Tonya Currie & her team resides in Washington & Grimes County areas and represent some of the most fascinating properties throughout Texas which have been featured in the Wall Street Journal, Paper City, Houston Luxury, & Houston Chronicle.',
-			type: 'Highly likely to recommend'
-		},
-		{
-			name: 'John Baker',
-			date: '06/01/2021',
-			ratings: '4.5',
-			comment:
-				'Tonya Currie & her team resides in Washington & Grimes County areas and represent some of the most fascinating properties throughout Texas which have been featured in the Wall Street Journal, Paper City, Houston Luxury, & Houston Chronicle.',
-			type: 'Highly likely to recommend'
-		}
-	];
+	export let profileData;
+	// console.log(profileData, 'review profileData');
+	var reviews = [];
 	let openAddReview = false;
 
 	function AddReview() {
-		if ($userdata) {
+		if (!$userdata) {
 			openSignup = true;
 		} else {
 			openAddReview = true;
 		}
 	}
-
+	async function getreviews() {
+		let { data: reviews, error } = await supabase
+			.from('reviews')
+			.select('*')
+			.eq('agent_id', profileData.profiles_id);
+		return reviews;
+	}
+	getreviews().then((x) => {
+		// brokerage_address = x.adress;
+		reviews = x;
+		console.log(x, 'getreviews');
+	});
 </script>
 
 <div class="text-sm card p-4">
@@ -63,45 +51,49 @@
 		{/if}
 	</div>
 	<hr />
-	{#if reviews.length < 1}
-		<div class="w-full text-center">
-			<div class="bg-primary-200 dark:bg-primary-500 p-4 rounded-lg m-4">
-				<EmptyReviews />
-			</div>
-			<p class="m-auto mt-6 text-xs dark:text-primary-300 text-primary-500">No Reviews Yet</p>
-		</div>
-	{/if}
-	{#each reviews as r, i}
-		<div class="my-4">
-			<div class="flex justify-between mb-4 items-center font-semibold">
-				<div>
-					<div>{r.name}</div>
-					<div class="text-sm text-surface-900 dark:text-surface-100 font-normal">{r.date}</div>
+	{#await reviews}
+		Loading
+	{:then}
+		{#if reviews.length < 1}
+			<div class="w-full text-center">
+				<div class="bg-primary-200 dark:bg-primary-500 p-4 rounded-lg m-4">
+					<EmptyReviews />
 				</div>
-				<div>
-					<div>{r.type}</div>
-					<div class="flex gap-2 float-right">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							class="ionicon w-4 text-primary-500 dark:text-surface-300"
-							fill="currentColor"
-							viewBox="0 0 512 512"
-							><path
-								d="M394 480a16 16 0 01-9.39-3L256 383.76 127.39 477a16 16 0 01-24.55-18.08L153 310.35 23 221.2a16 16 0 019-29.2h160.38l48.4-148.95a16 16 0 0130.44 0l48.4 149H480a16 16 0 019.05 29.2L359 310.35l50.13 148.53A16 16 0 01394 480z"
-							/></svg
-						>
-						<div>{r.ratings}</div>
+				<p class="m-auto mt-6 text-xs dark:text-primary-300 text-primary-500">No Reviews Yet</p>
+			</div>
+		{/if}
+		{#each reviews as r, i}
+			<div class="my-4">
+				<div class="flex justify-between mb-4 items-center font-semibold">
+					<div>
+						<div>{r.client_name}</div>
+						<div class="text-sm text-surface-900 dark:text-surface-100 font-normal">{r.date}</div>
+					</div>
+					<div>
+						<div>{r.type}</div>
+						<div class="flex gap-2 float-right">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								class="ionicon w-4 text-primary-500 dark:text-surface-300"
+								fill="currentColor"
+								viewBox="0 0 512 512"
+								><path
+									d="M394 480a16 16 0 01-9.39-3L256 383.76 127.39 477a16 16 0 01-24.55-18.08L153 310.35 23 221.2a16 16 0 019-29.2h160.38l48.4-148.95a16 16 0 0130.44 0l48.4 149H480a16 16 0 019.05 29.2L359 310.35l50.13 148.53A16 16 0 01394 480z"
+								/></svg
+							>
+							<div>{r.rating}</div>
+						</div>
 					</div>
 				</div>
+				<div>
+					{r.review}
+				</div>
 			</div>
-			<div>
-				{r.comment}
-			</div>
-		</div>
-		{#if i + 1 !== reviews.length}
-			<hr />
-		{/if}
-	{/each}
+			{#if i + 1 !== reviews.length}
+				<hr />
+			{/if}
+		{/each}
+	{/await}
 </div>
 <Model bind:show={openAddReview} width="w-fit max-md:mx-5">
 	<div slot="title">Add Reviews</div>
@@ -139,4 +131,4 @@
 		</div>
 	</div>
 </Model>
-<Signup  bind:show={openSignup}/>
+<Signup bind:show={openSignup} />
