3aaf4337bb6a3a5d2a3003473442972958792df3
AR-92 <electro.electra@gmail.com>
2023-09-23 14:50:05 +0500

    username removed

--- src/routes/api/your/+server.js
+++ src/routes/api/your/+server.js
@@ -3,7 +3,7 @@ import { supabase } from '$lib/supabaseClient';
 async function getdata(id) {
     const { data: profile, error } = await supabase
         .from('profile')
-        .select('profiles_id,auth_id,website_link,external_link,role,oa,avatar, name, rating, contact, username, dob, about, hobbies, education, experience, location_id(location), brokerage_id(name,address,id),service_areas, language(language,id),new,email,banner,avatar')
+        .select('profiles_id,auth_id,website_link,external_link,role,oa,avatar, name, rating, contact,  dob, about, hobbies, education, experience, location_id(location), brokerage_id(name,address,id),service_areas, language(language,id),new,email,banner,avatar')
         .eq('auth_id', id);
     if (error) console.log(error)
     return profile
