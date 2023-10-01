ea7cf726a2e32d47cd0923958e115d919a3aedbc
AR-92 <electro.electra@gmail.com>
2023-10-01 11:16:04 +0500

    nav portal fixed

--- src/routes/allchat/[slug]/+page.js
+++ src/routes/allchat/[slug]/+page.js
@@ -1,5 +1,6 @@
 export async function load({ fetch, params }) {
-        const res = await fetch(`/api/allchat?id=${params.slug}`);
+        var p=params.slug.split("@")
+        const res = await fetch(`/api/allchat?id=${p[0]}&role=${p[1]}`);
         const data = await res.json();
         console.log(data,'data from chat server')
         return {data:data};
